48b5c7e04dbca34830fc1eb97551ef40fd68a921
Euphraith <maganjoeuphraith256@gmail.com>
2025-08-14 20:33:40 +0300

    initial commit

--- 1-Python-Fundamentals/battlegame.py
+++ 1-Python-Fundamentals/battlegame.py
@@ -41,4 +41,33 @@ while True:
         my_hp = elf_hp
         my_damage = elf_damage
         break
-    
+    elif character_choice == "3":
+        character = human
+        my_hp = human_hp
+        my_damage = human_damage
+        break
+    else:
+        print("Unknown character\n")
+
+print(f"\nYou have chosen the {character}!\n")
+
+while True:
+    # Player's Turn
+    dragon_hp = dragon_hp - my_damage
+    print(f"The {character} damaged the Dragon!")
+    print(f"The Dragon's HP is now {dragon_hp}.\n")
+
+    # Check for Dragon's Defeat
+    if dragon_hp <= 0:
+        print("The Dragon has lost the battle!")
+        break
+
+    # Dragon's Turn
+    my_hp = my_hp - dragon_damage
+    print(f"The Dragon damaged the {character}!")
+    print(f"The {character}'s HP is now {my_hp}.\n")
+
+    # Check for Character's Defeat
+    if my_hp <= 0:
+        print(f"The {character} has lost the battle!")
+        break
